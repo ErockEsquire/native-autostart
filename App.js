87f606795a7af67316a0bc8eadae74dd2b47041e
erocknine <erock.esquire@gmail.com>
2022-12-13 12:20:03 -0500

    Add main code

--- App.js
+++ App.js
@@ -1,11 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, BackHandler, Image, Dimensions } from "react-native";
+import { Audio } from "expo-av";
+import { useEffect, useState } from "react";
+import DoubleClick from "react-native-double-tap";
+import loader from "./assets/images/loader.gif";
+import logo from "./assets/images/mitsubishi.png";
+import { StatusBar } from "expo-status-bar";
 
 export default function App() {
+  const [sound, setSound] = useState();
+  const exit = () => {
+    BackHandler.exitApp();
+  };
+
+  async function playSound() {
+    console.log("Loading Sound");
+    const { sound } = await Audio.Sound.createAsync(
+      require("./assets/audio/welcome-highway.mp3"),
+      {},
+      (status) => {
+        if (status.didJustFinish) {
+          exit();
+        }
+      }
+    );
+    setSound(sound);
+
+    console.log("Playing Sound");
+    await sound.playAsync();
+  }
+
+  useEffect(() => {
+    playSound();
+  }, []);
+
+  useEffect(() => {
+    return sound
+      ? () => {
+          console.log("Unloading Sound");
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
+  const windowWidth = Dimensions.get("window").width;
+  const windowHeight = Dimensions.get("window").height;
+  let loaderStyle = {
+    height: "100%",
+    width: undefined,
+    aspectRatio: 1,
+    alignSelf: "center",
+    position: "absolute",
+    bottom: 0,
+    top: 0,
+  };
+  if (windowWidth > windowHeight) {
+    loaderStyle.height = undefined;
+    loaderStyle.width = "100%";
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <DoubleClick doubleTap={exit}>
+        <Image style={loaderStyle} source={loader} />
+        <Image source={logo} />
+      </DoubleClick>
     </View>
   );
 }
@@ -13,8 +71,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
